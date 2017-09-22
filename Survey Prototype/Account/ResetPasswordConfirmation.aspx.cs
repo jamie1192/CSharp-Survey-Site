ce09c00b662fd8a70d0fe5a7e84cdbeb6b9a37ec
jastley92 - Parallels <jastley92@gmail.com>
2017-09-22 18:16:00 +1000

    Add project files.

---
+++ Survey Prototype/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Survey_Prototype.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Survey Prototype/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Survey_Prototype.Startup))]
+namespace Survey_Prototype
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
